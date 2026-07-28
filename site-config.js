84be1eb73df51dd45673ede6bb072d314c81d01b
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-07-28 11:24:45 +0900

    Connect stock spreadsheet

--- site-config.js
+++ site-config.js
@@ -1,7 +1,7 @@
 window.orderAutoConfig = {
   // Googleスプレッドシートを「ウェブに公開」したCSV URLを入れると、
   // data/stock.csv の代わりにスプレッドシートの在庫が表示されます。
-  stockCsvUrl: "",
+  stockCsvUrl: "https://docs.google.com/spreadsheets/d/1-0spW3O34MprYWQKhwE20x42S4JhqgYRxetiaRaEHZw/export?format=csv&gid=1798557793",
 
   // ご提案例もスプレッドシート管理にする場合はCSV URLを入れます。
   exampleCsvUrl: "",
